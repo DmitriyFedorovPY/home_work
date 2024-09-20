6e8674f4d0f9841fd7e4206b2ba7b8e00d6a9900
DmitriyFedorovPY <dmitriyfedorovpy@yahoo.com>
2024-09-20 23:47:30 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,21 @@
+#первое условие
+first = 123
+second = 456
+third = 789
+if first == second and first == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+#второе условие
+first = 42
+second = 69
+third = 42
+if first == second and first == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
