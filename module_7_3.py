8b83c64c49c3b4c2eb0c5a3351349826910d9e56
Dmitriy Fedorov <dmitriyfedorovpy@yahoo.com>
2024-12-02 13:14:07 +0500

    module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -1,37 +1,31 @@
-class WordsFinder():
+class WordsFinder:
     def __init__(self, *file_names):
         self.file_names = file_names
 
     def get_all_words(self):
-        all_words = dict()
-        chage_singns = [',', '.', '=', '!', '?', ';', ':', ' - ']
+        all_words = {}
         for file_name in self.file_names:
+            list = []
             with open(file_name, encoding='utf-8') as file:
-                words = []
                 for line in file:
-                    for sign in chage_singns:
-                        line = line.replace(sign, ' ')
-                    line = line.lower()
-                    for word in line.split():
-                        words.append(word)
-                file.close()
-            all_words[file_name] = words
+                    line_cls = line.lower().replace(',', '').replace('.', '').replace('=', '').replace('!', '').replace(
+                        '?', '').replace(';', '').replace(':', '').replace('-', '').split()
+                    list.append(line_cls)
+            all_words[file_name] = sum(list, [])
         return all_words
 
-    def find(self,word):
-        dict_of_words=dict()
-        for name,words in self.get_all_words().items():
-            if word.lower() in words:
-                dict_of_words[name] = words.index(word.lower()) + 1
-        return dict_of_words
-
+    def find(self, word): # метод реализован без проверки на ошибку
+        find_dict = {}
+        for name, words in self.get_all_words().items():
+           find_dict[name] = words.index(word.lower()) + 1
+        return find_dict
 
     def count(self, word):
-        dict_of_counts = dict()
-        for name,words in self.get_all_words().items():
-            if word.lower() in words:
-                dict_of_counts[name] = words.index(word.lower()) + 1
-        return dict_of_counts
+        count_dict = {}
+        for name, words in self.get_all_words().items():
+            count_dict[name] = words.count(word.lower())
+        return count_dict
+
 
 
 finder2 = WordsFinder('test_file.txt')
